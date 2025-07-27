c9acce2cd01da0fcdedbc902a71a679a4f1545ae
SanseL <sanselguh@gmail.com>
2025-07-27 21:57:31 +0700

    Add files via upload

--- cube-room/src/main.rs
+++ cube-room/src/main.rs
@@ -9,7 +9,7 @@ struct Placeholder;
 fn main() {
     App::new()
         .add_plugins( DefaultPlugins )
-        .add_plugins( ( bevy_spectator::SpectatorPlugin, bevy_egui::EguiPlugin::default(), bevy_inspector_egui::quick::WorldInspectorPlugin::new() ) )
+        .add_plugins( ( bevy_egui::EguiPlugin::default(), bevy_inspector_egui::quick::WorldInspectorPlugin::new() ) )
         .add_systems( Startup, setup )
         .add_systems( Update, ( movement, camera_following ) )
         .run();
@@ -33,14 +33,28 @@ fn setup(
             shadows_enabled: true,
             ..default()
         }, 
-        Camera3d::default(), bevy_spectator::Spectator,
-        Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
+        Camera3d::default(),
+        Transform {
+            translation: Vec3::new( 0., 2., 2.5 ),
+            rotation: Quat::from_rotation_x( -0.5 ),
+            ..default()
+        },
     ));
 }
 
-fn camera_following( player_cube: Query< &mut Transform, With< PlayableCube > >, camera: Query< &mut Transform, (With<Camera3d>, Without<PlayableCube>) >  ) {
+fn camera_following( player_cube: Query< &mut Transform, With< PlayableCube > >, mut camera: Query< &mut Transform, (With<Camera3d>, Without<PlayableCube>) >  ) {
+    let player_transform = player_cube.single().unwrap();
+    let mut camera_transform = camera.single_mut().unwrap();
 
+    // if camera_transform.translation.x == player_transform.translation.x {
+    //     return;
+    // }
+
+    camera_transform.translation.x = player_transform.translation.x;
+    camera_transform.translation.y = player_transform.translation.y + 2.;
+    camera_transform.translation.z = player_transform.translation.z + 3.5 ;
 }
 
 fn movement( player_cube: Query< &mut Transform, With<PlayableCube> > ) {
+
 }
